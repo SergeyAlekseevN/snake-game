bc9bed64e73d70399eb43299346c83397d18ab66
Sergey Alekseev <sergey@alekseev.bz>
2019-09-26 13:10:12 +0300

    add sample settings

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-settings',
@@ -6,8 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
+  gameModes = [
+    "normal",
+    "inverse keys",
+    "manual"
+  ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
