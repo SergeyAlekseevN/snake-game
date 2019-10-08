99631cd486b1324be3f784d2e4e133c4e029c1c9
Sergey Alekseev <sergey@alekseev.bz>
2019-10-08 23:27:15 +0300

    add scaleX scaleY to game settings

--- src/app/game/engine/game.settings.ts
+++ src/app/game/engine/game.settings.ts
@@ -1,15 +1,19 @@
 import {Direction} from "./direction.enum";
 
 export class GameSettings {
+  public scaleX: number = 64;
+  public scaleY: number = 64;
+
   constructor(
     public width: number,
     public height: number,
-    public scale: number,
+    public rows: number,
+    public cols: number,
     public fps: number,
     public snakeSettings?: SnakeSettings
   ) {
     if (this.snakeSettings === undefined || this.snakeSettings === null) {
-      this.snakeSettings = new SnakeSettings(4, width / scale / 2, height / scale / 2, Direction.RIGHT);
+      this.snakeSettings = new SnakeSettings(4, Math.floor(cols / 2), Math.floor(rows / 2), Direction.RIGHT);
     }
   }
 }
