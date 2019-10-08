0932189bc40acd762cd1775b7acf33428d7705b8
Sergey Alekseev <sergey@alekseev.bz>
2019-10-08 23:20:08 +0300

    add shapes adn colors for food

--- src/app/game/engine/sprites/food.sprite.ts
+++ src/app/game/engine/sprites/food.sprite.ts
@@ -8,14 +8,66 @@ import {GameSettings} from "../game.settings";
  */
 export class Food extends GameSprite {
   public coord: Vector;
+  public shape: Shape = Shape.square;
+  public color: Color = Color.RED;
 
   constructor(p: p5, public settings: GameSettings) {
     super(p);
   }
 
   draw(p: p5): void {
-    p.fill('red');
-    p.rect(this.coord.x, this.coord.y, this.settings.scale, this.settings.scale);
+    if (this.color === Color.RED) {
+      p.fill('red');
+    } else if (this.color === Color.BLUE) {
+      p.fill('blue');
+    } else if (this.color === Color.YELLOW) {
+      p.fill('yellow');
+    } else if (this.color === Color.GREEN) {
+      p.fill('green')
+    } else {
+      console.log("color for food not set");
+      p.fill('black');
+    }
+
+    const scaleX = this.settings.scaleX;
+    const scaleY = this.settings.scaleY;
+    const y = this.coord.y;
+    const x = this.coord.x;
+
+    if (this.shape === Shape.square) {
+      p.rect(x * scaleX, y * scaleY, scaleX, scaleY);
+    } else if (this.shape === Shape.circle) {
+      p.ellipse(x * scaleX, y * scaleY, scaleX, scaleY);
+    } else if (this.shape === Shape.triangle) {
+      p.triangle(
+        //up
+        x * scaleX + Math.floor(scaleX / 2),
+        y * scaleY,
+        //left
+        x * scaleX,
+        y * scaleY + scaleY,
+
+        //right
+        x * scaleX + scaleX,
+        y * scaleY + scaleY
+      )
+    } else if (this.shape === Shape.cross) {
+      //vertical
+      p.rect(
+        x * scaleX + Math.floor(scaleX / 4),
+        y * scaleY,
+        scaleX - Math.floor(scaleX / 2),
+        scaleY
+      );
+
+      //horizontal
+      p.rect(
+        x * scaleX,
+        y * scaleY + Math.floor(scaleY / 4),
+        scaleX,
+        scaleY - Math.floor(scaleY / 2)
+      );
+    }
   }
 
   update(p: p5): void {
@@ -25,4 +77,18 @@ export class Food extends GameSprite {
   putOnNewPlace(newPlace: Vector): void {
     this.coord = newPlace;
   }
+}
+
+export enum Color {
+  RED,
+  BLUE,
+  GREEN,
+  YELLOW
+}
+
+export enum Shape {
+  square,
+  triangle,
+  circle,
+  cross
 }
