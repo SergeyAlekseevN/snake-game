e75ae7cdcf53392a6d9e909fbea438d2b12010cb
Sergey Alekseev <sergey@alekseev.bz>
2019-10-16 00:40:12 +0300

    add firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDRty2-2j2O-cuGGGFgN6C-Q6xPTapahYs",
+    authDomain: "joker-snake.firebaseapp.com",
+    databaseURL: "https://joker-snake.firebaseio.com",
+    projectId: "joker-snake",
+    storageBucket: "joker-snake.appspot.com",
+    messagingSenderId: "762989063223",
+    appId: "1:762989063223:web:9d3637c4057aed3f6ee3b2",
+    measurementId: "G-7J5WWF33LB"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDRty2-2j2O-cuGGGFgN6C-Q6xPTapahYs",
+    authDomain: "joker-snake.firebaseapp.com",
+    databaseURL: "https://joker-snake.firebaseio.com",
+    projectId: "joker-snake",
+    storageBucket: "joker-snake.appspot.com",
+    messagingSenderId: "762989063223",
+    appId: "1:762989063223:web:9d3637c4057aed3f6ee3b2",
+    measurementId: "G-7J5WWF33LB"
+  }
 };
 
 /*
