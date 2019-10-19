d7efbe0f47814cb3af61b01967b79da081d0494a
Sergey Alekseev <sergey@alekseev.bz>
2019-10-19 18:36:49 +0300

    add security rules and enable auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,20 @@ import {RouterModule, Routes} from '@angular/router';
 import {LeaderboardComponent} from './leaderboard/leaderboard.component';
 import {GameComponent} from './game/game.component';
 import {SettingsComponent} from './settings/settings.component';
+import {AuthGuard} from "./core/auth.guard";
+import {LoginComponent} from "./login/login.component";
 
 const routes: Routes = [
   {path: '', redirectTo: 'game', pathMatch: 'full'},
-  {path: 'game', component: GameComponent},
-  {path: 'leaderboard', component: LeaderboardComponent},
-  {path: 'settings', component: SettingsComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'game', component: GameComponent, data: {allowedRoles: ['admin', 'player']}, canActivate: [AuthGuard]},
+  {
+    path: 'leaderboard',
+    component: LeaderboardComponent,
+    data: {allowedRoles: ['admin', 'player']},
+    canActivate: [AuthGuard]
+  },
+  {path: 'settings', component: SettingsComponent, data: {allowedRoles: ['admin']}, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
