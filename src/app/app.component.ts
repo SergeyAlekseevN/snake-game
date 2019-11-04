9d140578542245a6f4abe35273af06c0cb06e625
Sergey Alekseev <sergey@alekseev.bz>
2019-11-04 16:51:35 +0300

    enable firebase analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,12 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as firebase from "firebase";
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    firebase.analytics();
+  }
 }
